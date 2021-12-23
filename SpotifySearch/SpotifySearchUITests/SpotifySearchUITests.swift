c59a7a4e4704486ea84e879a36a8c9cb36f6832f
chakane <cs499@njit.edu>
2021-12-23 13:34:35 -0500

    init

---
+++ SpotifySearch/SpotifySearchUITests/SpotifySearchUITests.swift
@@ -0,0 +1,10 @@
+//
+//  SpotifySearchUITests.swift
+//  SpotifySearchUITests
+//
+//  Created by Chakane Shegog on 12/23/21.
+//
+
+import XCTest
+
+class SpotifySearchUITests: XCTestCase {}
---
+++ SpotifySearch/SpotifySearchUITests/SpotifySearchUITestsLaunchTests.swift
@@ -0,0 +1,10 @@
+//
+//  SpotifySearchUITestsLaunchTests.swift
+//  SpotifySearchUITests
+//
+//  Created by Chakane Shegog on 12/23/21.
+//
+
+import XCTest
+
+class SpotifySearchUITestsLaunchTests: XCTestCase {}
