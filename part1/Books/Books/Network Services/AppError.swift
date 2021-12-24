d70ed0b63741ae384c9d25fb5bba33f4ffafc8f9
chakane3 <39193557+chakane3@users.noreply.github.com>
2021-12-23 21:32:53 -0500

    app error

---
+++ part1/Books/Books/Network Services/AppError.swift
@@ -0,0 +1,18 @@
+//
+//  AppError.swift
+//  Books
+//
+//  Created by Chakane Shegog on 12/23/21.
+//
+
+import Foundation
+
+enum AppError: Error {
+    case badURL(String)
+    case noResponse
+    case networkClientError(Error)
+    case noData
+    case decodingError(Error)
+    case badStatusCode(Int)
+    case badMimeType(String)
+}
