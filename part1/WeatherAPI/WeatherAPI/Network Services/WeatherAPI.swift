55ad7068f5cf6b46a44f2c15ca310ba2518a1db7
chakane3 <39193557+chakane3@users.noreply.github.com>
2021-12-23 22:28:36 -0500

    init

---
+++ part1/WeatherAPI/WeatherAPI/Network Services/WeatherAPI.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherAPI.swift
+//  WeatherAPI
+//
+//  Created by Chakane Shegog on 12/23/21.
+//
+
+import Foundation
+
+struct WeatherAPI {
+    
+    // this function takes in a search query from a user and return us a weather object
+    // It uses a completion handler to handle the network request as we compile our data
+    
+}
