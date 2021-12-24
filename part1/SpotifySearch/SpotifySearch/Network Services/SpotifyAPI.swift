ebfe574b4671192ae4a49498ad8e3fb6f0057362
chakane3 <39193557+chakane3@users.noreply.github.com>
2021-12-23 20:48:17 -0500

    deleted secrets

--- part1/SpotifySearch/SpotifySearch/Network Services/SpotifyAPI.swift
+++ part1/SpotifySearch/SpotifySearch/Network Services/SpotifyAPI.swift
@@ -10,7 +10,7 @@ import Foundation
 struct SpotifyAPI {
     // this function takes in a search query of type string. this is what the user will enter in (input for endpoint)
     // it uses a completion handler to handle the network request as we compile our data
-    static func fetchRecipe(for searchQuery: String, completion: @escaping (Result<[Artists], AppError>) -> ()) {
-        
-    }
+//    static func fetchRecipe(for searchQuery: String, completion: @escaping (Result<[Artists], AppError>) -> ()) {
+//        
+//    }
 }
