d652deb1d6ca1f2d6a88a1153d0dcf115f7347fa
chakane3 <39193557+chakane3@users.noreply.github.com>
2021-12-23 22:51:55 -0500

    init

--- part1/WeatherAPI/WeatherAPI/Models/WeatherModel.swift
+++ part1/WeatherAPI/WeatherAPI/Models/WeatherModel.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+struct place: Codable {
+    let location: [placeLocation]
+}
+
+struct placeLocation: Codable {
+    let country: String
+    let current: [currentConditions]
+}
+
+struct currentConditions: Codable {
+    let condition: [conditionDict]
+}
+
+struct conditionDict: Codable {
+    let text: String
+}
+
+extension place {
+    static func getWeather(from jsonData: Data) throws -> place {
+        do {
+            let weatherFromPlace = try JSONDecoder().decode(place.self, from: jsonData)
+            return weatherFromPlace
+        } catch {
+            throw error
+        }
+    }
+}
